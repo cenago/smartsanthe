af3505291efd68ce003df18063318b278770db70
Ela <srisaiela@gmail.com>
2023-12-25 01:03:22 +0530

    Jenkins changes

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-ssh -tt root@165.22.213.30 <<EOF
+ssh root@165.22.213.30 <<EOF
   cd smartsanthe
-  git pull
-  source /root/smartsanthe/ssenv/bin/activate
-  pip install -r requirements.txt
-  ./manage.py makemigrations
-  ./manage.py migrate
-  sudo systemctl restart nginx
-  sudo service gunicorn restart
-  sudo service nginx restart
+#  git pull
+#  source /root/smartsanthe/ssenv/bin/activate
+#  pip install -r requirements.txt
+#  ./manage.py makemigrations
+#  ./manage.py migrate
+#  sudo systemctl restart nginx
+#  sudo service gunicorn restart
+#  sudo service nginx restart
   exit
 EOF
