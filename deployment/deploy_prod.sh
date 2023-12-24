63422c286e4577accaef7e434bb260870ef212da
Ela <srisaiela@gmail.com>
2023-12-24 22:53:30 +0530

    Jenkins changes

---
+++ deployment/deploy_prod.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ssh root@165.22.213.30 <<EOF
+  cd smartsanthe
+  git pull
+  source /root/smartsanthe/ssenv/bin/activate
+  pip install -r requirements.txt
+  ./manage.py makemigrations
+  ./manage.py migrate --run-syncdb
+  sudo systemctl restart nginx
+  sudo service gunicorn restart
+  sudo service nginx restart
+  exit
+EOF
--- smartsanthe/settings.py
+++ smartsanthe/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,10 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'C:\\Users\\Admin\\PycharmProjects\\sss\\smartsanthe\\smartsanthe\\static'
-STATIC_URL = 'static/'
 
-
+STATIC_URL = '/static/'
+sd = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = sd[0]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
